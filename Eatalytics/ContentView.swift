09fbd079a85f11d09af5ef108535fb52543cb853
Hemanth Nagubhai <hemanthnr@icloud.com>
2023-06-14 17:37:18 -0500

    Third Commit

--- Eatalytics/ContentView.swift
+++ Eatalytics/ContentView.swift
@@ -9,29 +9,55 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack(spacing:50) {
-            
-            Text("SEAMLESS START TO YOUR WELLNESS JOURNEY")
-                .fontWeight(.heavy)
-                .multilineTextAlignment(.leading)
-                .font(.custom(
-                        "Urbanist",
-                        fixedSize: 45))
-                .padding(.leading, 4.0)
-            
-            Text("Embark on your wellness journey with ease. With an intuitive app experience, say goodbye to complexity and hello to a smoother path towards your goals.")
-                .fontWeight(.regular)
-                .multilineTextAlignment(.leading)
-                .font(.custom(
-                        "Urbanist",
-                        fixedSize: 25))
-                .padding(.leading, 4.0)
-                
+        VStack(spacing: 50) {
             Spacer()
+            
+            HStack {
+                Spacer()
                 
-             
+                Text("SEAMLESS START TO YOUR WELLNESS JOURNEY")
+                    .fontWeight(.heavy)
+                    .foregroundColor(Color("Dark"))
+                    .multilineTextAlignment(.leading)
+                    .font(.custom("Urbanist", fixedSize: 45))
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.leading, 5)
+            }
+            
+            HStack {
+                Spacer()
+                
+                Text("Embark on your wellness journey with ease. With an intuitive app experience, say goodbye to complexity and hello to a smoother path towards your goals.")
+                    .foregroundColor(Color("Dark"))
+                    .fontWeight(.regular)
+                    .multilineTextAlignment(.leading)
+                    .font(.custom("Urbanist", fixedSize: 25))
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.leading, 5)
+            }
+            
+            Spacer()
+            
+            HStack {
+                Spacer()
+                Button(action: {
+                    // Action
+                }) {
+                    Image(systemName: "arrow.right")
+                        .font(.system(size: 24))
+                        .foregroundColor(.white)
+                        .padding(.all, 30.0)
+                        .background(Color("Dark"))
+                        .clipShape(Circle())
+                }
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .bottomTrailing)
+            .padding(.horizontal)
         }
         .padding()
+        .background(Color.white)
+        .edgesIgnoringSafeArea(.all)
+        .fixedSize(horizontal: false, vertical: true) // Allow text to expand vertically
     }
 }
 
@@ -40,3 +66,6 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
+
