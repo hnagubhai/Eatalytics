4efa6ffdf6d1d3f51b8dbff0949a9f76a6105902
Hemanth Nagubhai <hemanthnr@icloud.com>
2023-06-14 16:46:59 -0500

    Second commit

--- Eatalytics/ContentView.swift
+++ Eatalytics/ContentView.swift
@@ -9,9 +9,27 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
+        VStack(spacing:50) {
+            
             Text("SEAMLESS START TO YOUR WELLNESS JOURNEY")
+                .fontWeight(.heavy)
                 .multilineTextAlignment(.leading)
+                .font(.custom(
+                        "Urbanist",
+                        fixedSize: 45))
+                .padding(.leading, 4.0)
+            
+            Text("Embark on your wellness journey with ease. With an intuitive app experience, say goodbye to complexity and hello to a smoother path towards your goals.")
+                .fontWeight(.regular)
+                .multilineTextAlignment(.leading)
+                .font(.custom(
+                        "Urbanist",
+                        fixedSize: 25))
+                .padding(.leading, 4.0)
+                
+            Spacer()
+                
+             
         }
         .padding()
     }
