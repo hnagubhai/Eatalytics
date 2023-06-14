e3b50f2ab330bd5362f8a12a5ffd3700f4e811ac
Hemanth Nagubhai <hemanthnr@icloud.com>
2023-06-14 17:47:04 -0500

    Fourth commit

--- Eatalytics/ContentView.swift
+++ Eatalytics/ContentView.swift
@@ -9,55 +9,58 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack(spacing: 50) {
-            Spacer()
+        ZStack {
+            Color("Light")
+                .ignoresSafeArea()
             
-            HStack {
+            VStack(spacing: 50) {
                 Spacer()
                 
-                Text("SEAMLESS START TO YOUR WELLNESS JOURNEY")
-                    .fontWeight(.heavy)
-                    .foregroundColor(Color("Dark"))
-                    .multilineTextAlignment(.leading)
-                    .font(.custom("Urbanist", fixedSize: 45))
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                    .padding(.leading, 5)
-            }
-            
-            HStack {
+                HStack {
+                    Spacer()
+                    
+                    Text("SEAMLESS START TO YOUR WELLNESS JOURNEY")
+                        .fontWeight(.heavy)
+                        .foregroundColor(Color("Dark"))
+                        .multilineTextAlignment(.leading)
+                        .font(.custom("Urbanist", fixedSize: 45))
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .padding(.leading, 5)
+                }
+                
+                HStack {
+                    Spacer()
+                    
+                    Text("Embark on your wellness journey with ease. With an intuitive app experience, say goodbye to complexity and hello to a smoother path towards your goals.")
+                        .foregroundColor(Color("Dark"))
+                        .fontWeight(.regular)
+                        .multilineTextAlignment(.leading)
+                        .font(.custom("Urbanist", fixedSize: 25))
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .padding(.leading, 5)
+                }
+                
                 Spacer()
                 
-                Text("Embark on your wellness journey with ease. With an intuitive app experience, say goodbye to complexity and hello to a smoother path towards your goals.")
-                    .foregroundColor(Color("Dark"))
-                    .fontWeight(.regular)
-                    .multilineTextAlignment(.leading)
-                    .font(.custom("Urbanist", fixedSize: 25))
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                    .padding(.leading, 5)
-            }
-            
-            Spacer()
-            
-            HStack {
-                Spacer()
-                Button(action: {
-                    // Action
-                }) {
-                    Image(systemName: "arrow.right")
-                        .font(.system(size: 24))
-                        .foregroundColor(.white)
-                        .padding(.all, 30.0)
-                        .background(Color("Dark"))
-                        .clipShape(Circle())
+                HStack {
+                    Spacer()
+                    Button(action: {
+                        // Action
+                    }) {
+                        Image(systemName: "arrow.right")
+                            .font(.system(size: 24))
+                            .foregroundColor(Color("Light"))
+                            .padding(.all, 30.0)
+                            .background(Color("Dark"))
+                            .clipShape(Circle())
+                    }
+                    .padding(.bottom)
                 }
             }
-            .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .bottomTrailing)
-            .padding(.horizontal)
+            .padding()
+            .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
         }
-        .padding()
-        .background(Color.white)
         .edgesIgnoringSafeArea(.all)
-        .fixedSize(horizontal: false, vertical: true) // Allow text to expand vertically
     }
 }
 
