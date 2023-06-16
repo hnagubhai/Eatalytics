91c3cf1f7c6676b7ae9c4a673d9ae5c9368ea4d6
Hemanth Nagubhai <hemanthnr@icloud.com>
2023-06-15 23:05:29 -0500

    signin (buggy)

--- Eatalytics/Signup.swift
+++ Eatalytics/Signup.swift
@@ -13,29 +13,39 @@ struct Signup: View {
     @State private var password: String = ""
     
     var body: some View {
-        Form {
-            Section {
-                TextField("Email", text: $emailAddress)
-                    .textContentType(.emailAddress)
-                    .textInputAutocapitalization(.never)
-                    .disableAutocorrection(true)
-                    .keyboardType(.emailAddress)
-                SecureField("Password", text: $password)
-                    .textContentType(.password)
-                    .keyboardType(.default)
-            }
-            Section {
-                Button(action: {
-                    authModel.signUp(emailAddress: emailAddress, password: password)
-                }) {
-                    Text("Sign Up")
-                        .bold()
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Email", text: $emailAddress)
+                        .textContentType(.emailAddress)
+                        .keyboardType(.emailAddress)
+                }
+                Section {
+                    SecureField("Password", text: $password)
+                        .textContentType(.password)
+                        .keyboardType(.default)
+                }
+                Section {
+                    Button(action: {
+                        authModel.signUp(emailAddress: emailAddress, password: password)
+                    }) {
+                        Text("Sign Up")
+                            .bold()
+                    }
+                }
+                Section {
+                    NavigationLink(destination: SignInView()) {
+                        Text("Already Have an Account?")
+                            .bold()
+                    }
                 }
             }
+            .navigationTitle("Sign Up")
         }
-        .navigationTitle("Sign Up")
     }
 }
+
+
 
 struct Signup_Previews: PreviewProvider {
     static var previews: some View {
