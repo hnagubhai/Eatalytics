d89fd496e085c0e640f1827be6ce9c7eb4749f03
Hemanth Nagubhai <hemanthnr@icloud.com>
2023-07-03 15:38:50 -0500

    Initial Commit

---
+++ Eatalytics2/Eatalytics2App.swift
@@ -0,0 +1,17 @@
+//
+//  Eatalytics2App.swift
+//  Eatalytics2
+//
+//  Created by Hemanth Nagubhai on 7/3/23.
+//
+
+import SwiftUI
+
+@main
+struct Eatalytics2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
