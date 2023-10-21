427d942ee665af37c998f7164cb8690bfb224643
Manas Jain <jainmanas0410@gmail.com>
2023-10-21 15:29:27 -0500

    adding new setting functionalities

--- Eatalytics2/Registration/AuthViewModel.swift
+++ Eatalytics2/Registration/AuthViewModel.swift
@@ -7,31 +7,40 @@
 
 import SwiftUI
 import FirebaseAuth
-//replacing this
+
 final class AuthViewModel: ObservableObject {
-    var user: User? {
-        didSet {
-            objectWillChange.send()
-        }
-    }
+    @Published var user: User?
     
     func listenToAuthState() {
         Auth.auth().addStateDidChangeListener { [weak self] _, user in
-            guard let self = self else {
-                return
-            }
+            guard let self = self else { return }
             self.user = user
         }
     }
     
-    func signUp(
-        emailAddress: String,
-        password: String
-    ) {
+    func signUp(emailAddress: String, password: String, displayName: String) {
         Auth.auth().createUser(withEmail: emailAddress, password: password) { result, error in
             if let error = error {
-                print("an error occured: \(error.localizedDescription)")
-                return
+                print("An error occurred: \(error.localizedDescription)")
+            } else {
+                // Update user's display name
+                if let user = Auth.auth().currentUser {
+                    let changeRequest = user.createProfileChangeRequest()
+                    changeRequest.displayName = displayName
+                    changeRequest.commitChanges { error in
+                        if let error = error {
+                            print("Failed to set display name: \(error.localizedDescription)")
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    func signIn(emailAddress: String, password: String) {
+        Auth.auth().signIn(withEmail: emailAddress, password: password) { result, error in
+            if let error = error {
+                print("An error occurred: \(error.localizedDescription)")
             }
         }
     }
@@ -40,7 +49,39 @@ final class AuthViewModel: ObservableObject {
         do {
             try Auth.auth().signOut()
         } catch let signOutError as NSError {
-            print("Error signing out: %@", signOutError)
+            print("Error signing out: \(signOutError)")
+        }
+    }
+    
+    func changeEmail(newEmail: String) {
+        if let user = Auth.auth().currentUser {
+            user.updateEmail(to: newEmail) { error in
+                if let error = error {
+                    print("Failed to change email: \(error.localizedDescription)")
+                }
+            }
+        }
+    }
+    
+    func changePassword(newPassword: String) {
+        if let user = Auth.auth().currentUser {
+            user.updatePassword(to: newPassword) { error in
+                if let error = error {
+                    print("Failed to change password: \(error.localizedDescription)")
+                }
+            }
+        }
+    }
+    
+    func changeDisplayName(newName: String) {
+        if let user = Auth.auth().currentUser {
+            let changeRequest = user.createProfileChangeRequest()
+            changeRequest.displayName = newName
+            changeRequest.commitChanges { error in
+                if let error = error {
+                    print("Failed to change display name: \(error.localizedDescription)")
+                }
+            }
         }
     }
 }
